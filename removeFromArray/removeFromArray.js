845f6adc0a18728db3b6149dd2d799cab4b66ed9
jobtye <joe.lander@ymail.com>
2021-05-11 10:20:29 +0200

    first attempt

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
-}
+const removeFromArray = function(wanted, notWanted) {
+    let numbers = Array.from(wanted);
+    
+    for(i = 10; i > 0; i--){
+        if(numbers[i] === notWanted){
+            let result = numbers.splice(i, 1);
+            return result;
+        }
+    } 
+}   
+  
 
 module.exports = removeFromArray
