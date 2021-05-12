78405dee2221d97cf4dfdcca504d1b6a5c45194a
jobtye <joe.lander@ymail.com>
2021-05-12 16:41:57 +0200

    To test on jasmine.

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(wanted, notWanted) {
+    
+    let wantedDone = Array.from(wanted);
+    let numberToRemove = wantedDone.indexOf(notWanted);
+   
+   
+    for (let i=2; i < arguments.length; i++) {
 
+        let args = wantedDone.indexOf(arguments[i]);
+        wantedDone.splice(args, 1);
+    }
+    
+    wantedDone.splice(numberToRemove, 1);
+
+    return wantedDone;
 }
-
+removeFromArray([1,2,3,4], 1, 4);
 module.exports = removeFromArray
