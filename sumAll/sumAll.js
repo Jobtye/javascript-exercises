713c3ab40a456b4ef56063db797fb2ca07adbfd4
jobtye <joe.lander@ymail.com>
2021-05-14 13:40:51 +0200

    Working with numbers. to test on jasmine

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(first, seccond) {
+    let finalArray = [];
+    let i = 0;
+    let finalNumber = 0;
+
+
+    for(let i = first; i <= seccond; i++ ){ //gets all numbers inbetween and makes an array
+        finalArray.push(i);
+        
+    }
+
+    while ( i < finalArray.length ){ //does all the adding. but loopoing through the string
+    finalNumber += finalArray[i] //kinda concats into the finalnumber variable.
+    i++;
+    }
+    return finalNumber;
 
 }
 
