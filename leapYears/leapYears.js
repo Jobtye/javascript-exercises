6c6d9132aad9c74ba5b8c7963c7a3ef3dbf1719e
jobtye <joe.lander@ymail.com>
2021-05-17 21:34:10 +0200

    Done

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,10 +1,11 @@
-const leapYears = function( yearInput ) {
+const leapYears = function( yearInput ){
 
-  if( yearInput % 100 === 0 && yearInput % 400 === 0 && yearInput % 4 === 0 ){
-    console.log ( "Leapy Year!" );
+  if( yearInput % 4 == 0 && yearInput % 100 !== 0 ){  //Check if divisable by 4 and not by 100
+    return true;
+  }else if( yearInput % 100 == 0 && yearInput % 400 == 0 ){ //if it is divisable by 100 then make sure 400 too. 
+    return true;
   }else{
-    console.log ('Not a leapy!' );
+    return false;
   }
 }
-
 module.exports = leapYears
