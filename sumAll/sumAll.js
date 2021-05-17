b6be09998d23984c63453a6fd8210f2491c46286
jobtye <joe.lander@ymail.com>
2021-05-17 14:18:22 +0200

    FINISHED

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -3,18 +3,29 @@ const sumAll = function(first, seccond) {
     let i = 0;
     let finalNumber = 0;
 
+    if ( typeof(first) !== typeof(seccond)){
+        return 'ERROR';
 
+    }else if (first < seccond ){
     for(let i = first; i <= seccond; i++ ){ //gets all numbers inbetween and makes an array
         finalArray.push(i);
-        
-    }
+      }
 
+    }else if (first > seccond){
+    for(let i = seccond; i <= first; i++ ){ //gets all numbers inbetween and makes an array
+        finalArray.push(i);
+    }
+}
     while ( i < finalArray.length ){ //does all the adding. but loopoing through the string
     finalNumber += finalArray[i] //kinda concats into the finalnumber variable.
     i++;
     }
+
+    if (finalNumber < 0) {
+        return 'ERROR';
+    }else {
+
     return finalNumber;
-
-}
-
+    }
+}   
 module.exports = sumAll
