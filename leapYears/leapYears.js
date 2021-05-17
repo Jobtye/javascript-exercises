386077ce6a2c566af3d7715c681dd791beae47e3
jobtye <joe.lander@ymail.com>
2021-05-17 20:54:36 +0200

    To test on Jasmine

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function( yearInput ) {
 
+  if( yearInput % 100 === 0 && yearInput % 400 === 0 && yearInput % 4 === 0 ){
+    console.log ( "Leapy Year!" );
+  }else{
+    console.log ('Not a leapy!' );
+  }
 }
 
 module.exports = leapYears
