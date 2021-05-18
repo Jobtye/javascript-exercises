050bda7aef888443bb1bed048980275980517c86
jobtye <joe.lander@ymail.com>
2021-05-18 10:18:47 +0200

    Finished

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,25 @@
-const ftoc = function() {
+const ftoc = function(fahrenheitTemp) {
+  
+  let calc = (fahrenheitTemp - 32) * (5/9);
 
+  if (calc % 1 !== 0){
+    return parseFloat(calc.toFixed(1));
+}else {
+  return  calc;
+  }
 }
 
-const ctof = function() {
+const ctof = function(cTemp) { 
+  
+  let calc2 = (cTemp * 9/5) + 32;
 
+  if (calc2 % 1 !== 0){
+    return parseFloat((calc2.toFixed(1)));
+  }else {
+    return calc2;
+  }
 }
+
 
 module.exports = {
   ftoc,
